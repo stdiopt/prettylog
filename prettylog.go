8940514b266f524e64d4c333a9030ac189a94acc
Luis Figueiredo <luisf@hexasoftware.com>
2017-05-02 21:38:04 -0300

    Added prefix

--- prettylog.go
+++ prettylog.go
@@ -9,15 +9,16 @@ import (
 )
 
 type PrettyLogWritter struct {
+	prefix   string
 	lastTime time.Time
 	counter  int64
 }
 
-func New() *PrettyLogWritter {
-	return &PrettyLogWritter{time.Now(), 0}
+func NewWriter(prefix string) *PrettyLogWritter {
+	return &PrettyLogWritter{prefix, time.Now(), 0}
 }
 
-func (this *PrettyLogWritter) Write(b []byte) (int, error) {
+func (p *PrettyLogWritter) Write(b []byte) (int, error) {
 
 	/*{
 		for i := 0; i < 6; i++ {
@@ -32,29 +33,30 @@ func (this *PrettyLogWritter) Write(b []byte) (int, error) {
 	li := strings.LastIndex(tname, "/")
 	fname := tname[li+1:]
 
-	timeDiff := time.Since(this.lastTime)
+	timeDiff := time.Since(p.lastTime)
 
 	var fduration float64 = float64(timeDiff.Nanoseconds()) / 1000000.0
 
-	msg := fmt.Sprintf("[%d:\033[34m%s\033[0m (\033[33m%s:%d\033[0m) \033[90m+%.2f/ms\033[0m]: %s",
-		this.counter,
+	msg := fmt.Sprintf("[%d:\033[34m%s\033[0m (\033[33m%s:%d\033[0m) <\033[35m%s\033[0m> \033[90m+%.2f/ms\033[0m]: %s",
+		p.counter,
 		time.Now().Format("2006-01-02 15:04:05"),
 		fname,
 		line,
+		p.prefix,
 		fduration,
 		string(b),
 	)
-	this.lastTime = time.Now()
-	this.counter++
+	p.lastTime = time.Now()
+	p.counter++
 
 	return fmt.Print(msg)
 }
 
-func CreateLogger() *log.Logger {
-	return log.New(New(), "", 0)
+func New(prefix string) *log.Logger {
+	return log.New(NewWriter(prefix), "", 0)
 }
 
 func Global() {
 	log.SetFlags(0)
-	log.SetOutput(New())
+	log.SetOutput(NewWriter(""))
 }
